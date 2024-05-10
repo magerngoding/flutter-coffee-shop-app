e52a16400a664c82992f1c28be667c5a365c272e
Hafizh <Hafizh@gmail.com>
2024-05-10 23:15:14 +0700

    Beans detail page 2

--- lib/presentation/home/pages/beans_detail_page.dart
+++ lib/presentation/home/pages/beans_detail_page.dart
@@ -1,9 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first, prefer_const_literals_to_create_immutables
 // ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables, prefer_const_constructors
 
-import 'package:coffee_shop_app/shared/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+
+import 'package:coffee_shop_app/shared/theme.dart';
 
 class BeanDetailPage extends StatelessWidget {
   const BeanDetailPage({Key? key}) : super(key: key);
@@ -13,6 +15,7 @@ class BeanDetailPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Stack(
               children: <Widget>[
@@ -228,7 +231,178 @@ class BeanDetailPage extends StatelessWidget {
                 ),
               ],
             ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 18),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Description",
+                    style: grayAETextStyle.copyWith(
+                      fontWeight: semiBold,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  Text(
+                    "Arabica beans are by far the most popular type of coffee beans, making up about 60% of the world’s coffee. These tasty beans originated many centuries ago in the highlands of Ethiopia, and may even be the first coffee beans ever consumed! ",
+                    style: whiteTextStyle.copyWith(
+                      fontSize: 14.0,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  Text(
+                    "Size",
+                    style: grayAETextStyle.copyWith(
+                      fontWeight: semiBold,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizeGramWidget(
+                        isActve: true,
+                        gram: 250,
+                        isColor: true,
+                      ),
+                      SizeGramWidget(
+                        gram: 500,
+                      ),
+                      SizeGramWidget(
+                        gram: 1000,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 28.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            "Price",
+                            style: grayAETextStyle.copyWith(
+                              fontWeight: medium,
+                              fontSize: 12.0,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 3.0,
+                          ),
+                          RichText(
+                            textAlign: TextAlign.center,
+                            text: TextSpan(
+                              text: '\$ ',
+                              style: brownTextStyle.copyWith(
+                                fontSize: 15,
+                                fontWeight: semiBold,
+                              ),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: '10.50',
+                                  style: whiteTextStyle.copyWith(
+                                    fontSize: 15.0,
+                                    fontWeight: semiBold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            width: 240,
+                            height: 60.0,
+                            decoration: BoxDecoration(
+                              color: brownColor,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(
+                                  20.0,
+                                ),
+                              ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Add to Cart",
+                                style: whiteTextStyle.copyWith(
+                                  fontSize: 16.0,
+                                  fontWeight: semiBold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 50.0,
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class SizeGramWidget extends StatelessWidget {
+  final bool isActve;
+  final bool isColor;
+  final int gram;
+  const SizeGramWidget({
+    Key? key,
+    this.isActve = false,
+    this.isColor = false,
+    required this.gram,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40.0,
+      width: 100,
+      decoration: BoxDecoration(
+        color: Color(0xff141921),
+        border: Border.all(
+          width: 1.0,
+          color: isActve ? brownColor : Color(0xff141921),
+        ),
+        borderRadius: BorderRadius.all(
+          Radius.circular(
+            10.0,
+          ),
+        ),
+      ),
+      child: Center(
+        child: Text(
+          ' ${gram.toString()}gm',
+          style: isColor
+              ? brownTextStyle.copyWith(
+                  fontSize: 12.0,
+                  fontWeight: medium,
+                )
+              : grayAETextStyle.copyWith(
+                  fontSize: 12.0,
+                  fontWeight: medium,
+                ),
         ),
       ),
     );
